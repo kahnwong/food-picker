1637a6c6943df26ab0468a4c05af635577bda32b
Karn Wong <karn@karnwong.me>
2025-01-17 22:39:49 +0700

    feat: init food picker

---
+++ src/components/FoodComponent.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="container">
+    <div class="food-random">
+      {{ randomMenu }}
+    </div>
+    <div class="food-all">
+      <span class="text-bold">ALL MENUS</span>
+      <ul>
+        <li v-for="(menu, index) in menus" :key="index">
+          {{ menu }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+
+interface MenuProps {
+  menus: string[]
+}
+const props = withDefaults(defineProps<MenuProps>(), {
+  menus: () => [],
+})
+
+const randomMenu = computed(() => {
+  const items = props.menus
+  return items[Math.floor(Math.random() * items.length)]
+})
+</script>
+
+<!--generated via gemini because I suck at anything ui-related-->
+<style>
+.container {
+  display: grid;
+  grid-template-rows: 1fr 2fr; /* Top row takes 1/3 height, bottom row takes 2/3 height */
+  justify-content: center;
+  align-items: center;
+  height: 70vh; /* viewport height */
+}
+
+.food-random {
+  text-align: center;
+}
+
+.food-all {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  list-style: none;
+  padding: 0;
+}
+
+.food-all li {
+  margin-bottom: 10px;
+}
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,43 +1,23 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+  <q-page class="row items-center justify-evenly q-pt-lg">
+    <food-component title="Example component" active :menus="menus"></food-component>
   </q-page>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
-import type { Todo, Meta } from 'components/models'
-import ExampleComponent from 'components/ExampleComponent.vue'
+import FoodComponent from 'components/FoodComponent.vue'
 
-const todos = ref<Todo[]>([
-  {
-    id: 1,
-    content: 'ct1',
-  },
-  {
-    id: 2,
-    content: 'ct2',
-  },
-  {
-    id: 3,
-    content: 'ct3',
-  },
-  {
-    id: 4,
-    content: 'ct4',
-  },
-  {
-    id: 5,
-    content: 'ct5',
-  },
+const menus = ref<string[]>([
+  'ข้าวอบหม้อดิน',
+  'ผัดไทย',
+  'ราดหน้า',
+  'สุกี้',
+  'หมี่สามย่าน',
+  'หมี่โอเดียน',
+  'หอยทอด',
+  'เตี๋ยวเป็ด',
+  'เย็นตาโฟ',
+  'แสนยอด',
 ])
-
-const meta = ref<Meta>({
-  totalCount: 1200,
-})
 </script>
